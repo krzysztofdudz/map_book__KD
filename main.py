c6a1ae6ce883510244f7da58de48c56c17988425
krzysztofdudz <u64725@student.wat.edu.pl>
2025-06-08 16:28:07 +0200

    chore: the code was restructured according to the mvc template

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
+from utils.model import users
+from utils.controller import get_user_info
 
 
-users:list=[
-    {'name':'Maciej','location':'Lódź','posts':100},
-    {'name':'Mateusz','location':'Poznań','posts':200},
-    {'name':'Maciej01','location':'Siedlce','posts':300},
-    {'name':'Konrad','location':'Grudziądz','posts':400},
-]
 
 
-def get_user_info(users_data:list)->None:
-    for user in users_data:
-        print(f'Twój znajomy {user["name"]} z miejscowosci {user['location']} opublikował {user["posts"]} postów. ')
+def main():
+    get_user_info(users)
 
-get_user_info(users)
+
+
+if __name__ == '__main__':
+    main()
